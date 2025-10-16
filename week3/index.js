e28a36d09c96d84c7c8beabc6b15aaa458fb8143
mdSamirali <sameerkhaan668@gmail.com>
2025-10-16 16:02:35 +0000

    push

--- week3/index.js
+++ week3/index.js
@@ -10,3 +10,15 @@ const EmpSkills= (skills)=> {
     console.log("Expert in "+ skills)
 }
 EmpSkills("java")
+
+
+const student= require('./StudentInfo')
+const person = require('./Person')
+// because getName is the function so we use ()
+console.log("Student Name:" +student.getName())
+console.log(student.Location())
+console.log(student.dob)
+// because dob is a variable so we do nt use ()
+console.log(student.Studentgrade())
+console.log("grade is "+student.Studentgrade(55) )
+// creating new Person

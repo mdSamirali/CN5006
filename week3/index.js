cc38be4d8e36a131193badbfbff71ccbcf53ef00
mdSamirali <sameerkhaan668@gmail.com>
2025-10-16 15:37:34 +0000

    push

--- week3/index.js
+++ week3/index.js
@@ -1,1 +1,12 @@
-
+function EmployeeInfo( name,Salary)
+{
+    console.log("Wellcome " + name+ " Your monthly salary is " + Salary)
+}
+console.log ("This is my first programe")
+var EmpName="John"
+var EmpSalary= 50000
+EmployeeInfo(EmpName,EmpSalary)
+const EmpSkills= (skills)=> {
+    console.log("Expert in "+ skills)
+}
+EmpSkills("java")
